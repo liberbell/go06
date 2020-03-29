54a5b6c59acc633beba973bd1e5e583d52aeb982
Liberbell <liberbell@gmail.com>
2020-03-29 16:22:02 +0900

    functions4

--- functions.go
+++ functions.go
@@ -10,6 +10,15 @@ func subt(x int, y int) int {
 	return x - y
 }
 
+func mult(a, b, string) (string, string) {
+	return a, b
+}
+
+// func main() {
+// 	fmt.Println(subt(18, 10))
+// }
+
 func main() {
-	fmt.Println(subt(18, 10))
+	x, y := mult("hello", "world")
+	fmt.Println(x, y)
 }
