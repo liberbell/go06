6ccff18e7ea55498beed79e26bf138b28d5f2e78
Liberbell <liberbell@gmail.com>
2020-03-29 16:39:43 +0900

    ifstatement6

--- ifstatement.go
+++ ifstatement.go
@@ -4,9 +4,10 @@ import "fmt"
 
 func main() {
 	for y := 1; y <= 5; y++ {
-		if y % 2 == 0 {
+		if y%2 == 0 {
 			fmt.Println(y, "even number")
 		} else {
 			fmt.Println(y, "old number")
 		}
+	}
 }
