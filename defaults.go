24881d9ced2b6d3089c9599fbbab03dbe0bdd3f3
Liberbell <liberbell@gmail.com>
2020-03-30 13:42:15 +0900

    defaults4

--- defaults.go
+++ defaults.go
@@ -1,10 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
 	switch time.Now().Weekday() {
 	case time.Monday, time.Tuesday, time.Wednesday, time.Thursday, time.Friday:
 		fmt.Println("It is a weekday.")
-	}
 	default:
 		fmt.Println("It is a weekend.")
+	}
 }
