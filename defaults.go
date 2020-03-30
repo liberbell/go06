8ff7029aa00b4ed40fc4a356c832b1ebcdab19b3
Liberbell <liberbell@gmail.com>
2020-03-30 13:45:13 +0900

    defaults5

--- defaults.go
+++ defaults.go
@@ -5,11 +5,20 @@ import (
 	"time"
 )
 
+// func main() {
+// 	switch time.Now().Weekday() {
+// 	case time.Monday, time.Tuesday, time.Wednesday, time.Thursday, time.Friday:
+// 		fmt.Println("It is a weekday.")
+// 	default:
+// 		fmt.Println("It is a weekend.")
+// 	}
+// }
+
 func main() {
-	switch time.Now().Weekday() {
-	case time.Monday, time.Tuesday, time.Wednesday, time.Thursday, time.Friday:
-		fmt.Println("It is a weekday.")
+	switch {
+	case time.Hour() < 12:
+		fmt.Println("Before noon.")
 	default:
-		fmt.Println("It is a weekend.")
+		fmt.Println("Afternoon.")
 	}
 }
