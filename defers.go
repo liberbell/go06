35f0f10087a0149806086c50495ebc77d564097f
Liberbell <liberbell@gmail.com>
2020-03-30 13:57:42 +0900

    defers3

--- defers.go
+++ defers.go
@@ -3,7 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	defer fmt.Println("World.")
+	// defer fmt.Println("World.")
 
-	fmt.Println("Hello ")
+	// fmt.Println("Hello ")
+	fmt.Println("counting")
+	for x := 0; x < 5; x++ {
+		defer fmt.Println(x)
+	}
+	fmt.Println("finished.")
 }
