3465320c1d8f06e5fdf6a4235e8689cf6e036519
Liberbell <liberbell@gmail.com>
2020-03-30 13:35:06 +0900

    swichtes1

--- switches.go
+++ switches.go
@@ -2,15 +2,27 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	s := 5
+// 	fmt.Println("This is: ", s)
+// 	switch s {
+// 	case 3:
+// 		fmt.Println("three")
+// 	case 4:
+// 		fmt.Println("four")
+// 	case 5:
+// 		fmt.Println("five")
+// 	}
+// }
+
 func main() {
-	s := 5
-	fmt.Println("This is: ", s)
-	switch s {
-	case 3:
-		fmt.Println("three")
-	case 4:
-		fmt.Println("four")
-	case 5:
-		fmt.Println("five")
+	lenght := 9
+	switch {
+	case lenght <= 7:
+		fmt.Println("Short")
+	case length <= 8:
+		fmt.Println("Normal")
+	case length > 8:
+		fmt.Println("Long")
 	}
 }
