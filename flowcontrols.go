3d43c88ec74ee24c18f70e3fc4f36bc4f4435575
Liberbell <liberbell@gmail.com>
2020-03-31 18:27:18 +0900

    flowcontrols3

--- flowcontrols.go
+++ flowcontrols.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	z := 0
-	for z <= 5 {
+	// z := 0
+	// for z <= 5 {
+	// 	fmt.Println(z)
+	// 	z = z + 1
+	// }
+	for z := 0; z <= 5; z++ {
 		fmt.Println(z)
-		z = z + 1
 	}
 }
